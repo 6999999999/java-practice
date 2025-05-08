1b9072b025c9d55f3bff3eb3d314f5352aebb175
ravindra <ravindra.hajare@techstalwarts.com>
2025-05-08 23:48:57 +0530

    personal practice

---
+++ stirngPrograms/StringPrograms.java
@@ -0,0 +1,10 @@
+package stirngPrograms;
+
+public class StringPrograms {
+    public static void main(String[] args) {
+        System.out.println("""
+                {sjdfka"" sjdfjsld"" }""");
+
+        System.out.println("   {sjdfka\"\" sjdfjsld\"\" }");
+    }
+}
